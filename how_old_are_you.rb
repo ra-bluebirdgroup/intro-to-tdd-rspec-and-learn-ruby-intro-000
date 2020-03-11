d728fd75452ceb0fdf727403bc1102f25ae0e16b
Ronald Abreu <ra@bluebirdgroup.ltd>
2020-03-11 17:35:21 +0000

    Automatically backed up by Learn

--- how_old_are_you.rb
+++ how_old_are_you.rb
@@ -6,4 +6,4 @@ birth_year = gets.to_i
 users_age = current_age_for_birth_year(birth_year)
 
 puts "You are: " + users_age.to_s + " years old."
-Run this program with ruby how_old_are_you.rb. There should
+
